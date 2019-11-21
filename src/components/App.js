111d8f21f50765661a4b2d815b8af4bf5f50a0c1
github username <github email address>
2019-11-21 15:20:14 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,53 @@ import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
-import Hog from './Hog'
-import HogsBrower from './HogsBrower'
+import HogContainer from './HogContainer'
+import Filter from './Filter'
 
 class App extends Component {
 
-  state={
-    hogs: hogs
+  constructor(){
+    super()
+    this.state = {
+      hogs: hogs
+    }
+  }
+
+  handleFilter = ()=>{
+    
+    let filtered = hogs.filter(hog => hog.greased === true )
+    
+    
+    this.setState({
+      hogs: filtered
+    })
+  }
+
+  sortByName = () => {
+    let sortedNames = hogs.sort((a, b) => a.name.localeCompare(b.name))
+  
+    this.setState({
+      hogs: sortedNames
+    })
+    
+  }
+  sortByWeight = () => {
+    let sortedWeight = hogs.sort((a, b) => a.weight - b.weight)
+  
+    this.setState({
+      hogs: sortedWeight
+    })
+    
   }
 
   render() {
     return (
       <div className="App">
           < Nav />
-          < HogsBrower
-            hogs={this.state.hogs}
-            // specialty={hog.specialty}
-            // greased={hog.greased}
-            // weight={hog.weight}
-            />
+          < Filter handleFilter={this.handleFilter} sortByName={this.sortByName} sortByWeight={this.sortByWeight}/>
+          <br/>
+          < HogContainer
+            hogs={this.state.hogs}/>
       </div>
     )
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,10 @@ import React from 'react'
 const Nav = () => {
 	return (
 		<div className="navWrapper">
+	
+		
+		
+		
 			<span className="headerText">Hogwarts</span>
 			<div className="TwirlyPig">
 				<img src={piggy} className="App-logo" alt="piggy" />
