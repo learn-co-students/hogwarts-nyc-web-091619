b5399a26fbcdab6c46cb01c75ffc77b374bfb724
Anya <anya.s.litvinova@gmail.com>
2019-11-21 12:07:41 -0500

    images

--- src/components/HogCard.js
+++ src/components/HogCard.js
@@ -3,16 +3,26 @@ import React, {Component} from 'react';
 
 export default class HogCard extends Component {
 
-  constructor(props){
-    super(props)
-    this.state = {
-      showDetails: false
-    }
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       showDetails: false
+//     }
+//   }
+
+  getImgPath(name){
+    let hogName = name.toLowerCase().split(' ').join('_')
+    console.log(hogName)
+       return require(`../hog-imgs/${hogName}.jpg`)
+      
   }
+  // src/hog-imgs/augustus_gloop.jpg
+
   render() {
     return (
       <div className = "ui card">
         <h1>{this.props.hog.name}</h1>
+        <img src={this.getImgPath(this.props.hog.name)} />
         </div>
     )}
 
